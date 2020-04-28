193ff4185584879bf700e0929f90bdeb1e9aba46
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-04-28 13:57:16 +0200

    feat(got): add user-agent
    disable cache for now until there is an own cache in place

--- source/hunter/got.ts
+++ source/hunter/got.ts
@@ -1,7 +1,15 @@
 import got from 'got'
 
-const cache = new Map<string, string>()
+import * as packageJson from '../../package.json'
+
+// TODO: implement cache
+// const cache = new Map<string, string>()
+// When using the cache got might return 304 but no content.
+// As its possible that users have the same url multiple times there should be a cache in the first place to prevent multiple calls to the same url
+// Also make sure to serve from own cache when 304 is returned
 
 export const cachedGot = got.extend({
-	cache
+	headers: {
+		'user-agent': 'WebsiteChangedBot ' + packageJson.version + ' https://github.com/EdJoPaTo/WebsiteChangedBot'
+	}
 })
