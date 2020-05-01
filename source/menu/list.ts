0d105458aed41caa4b1f788388ce78ebf943d6c4
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-05-01 19:50:35 +0200

    feat(list): show list in body too

--- source/menu/list.ts
+++ source/menu/list.ts
@@ -1,4 +1,5 @@
-import {MenuTemplate} from 'telegraf-inline-menu'
+import {html as format} from 'telegram-format'
+import {MenuTemplate, Body} from 'telegraf-inline-menu'
 
 import {generateFilename} from '../hunter'
 import {userMissions} from '../user-missions'
@@ -9,7 +10,7 @@ import {menu as detailsMenu} from './details'
 
 const SHOW_URL_LENGTH = 50
 
-export const menu = new MenuTemplate<Context>('Here you can see all the missions you set up to check regularly.')
+export const menu = new MenuTemplate<Context>(menuBody)
 
 menu.chooseIntoSubmenu('', ctx => getAllEntries(ctx.from!.id), detailsMenu, {
 	columns: 1,
@@ -20,6 +21,25 @@ menu.chooseIntoSubmenu('', ctx => getAllEntries(ctx.from!.id), detailsMenu, {
 })
 
 menu.manualRow(backButtons)
+
+function menuBody(context: Context): Body {
+	let text = ''
+
+	text += 'Here you can see all the missions you set up to check regularly.'
+	text += '\n\n'
+
+	const all = userMissions.get(`tg${context.from!.id}`) ?? []
+	const missions = all
+		.sort((a, b) => generateFilename(a.url, a.type).localeCompare(generateFilename(b.url, b.type)))
+
+	for (const mission of missions) {
+		const label = format.monospace(mission.type) + ': ' + format.escape(mission.url)
+		text += format.url(label, mission.url)
+		text += '\n'
+	}
+
+	return {text, parse_mode: format.parse_mode}
+}
 
 function getAllEntries(user: number): Map<string, string> {
 	const all = userMissions.get(`tg${user}`) ?? []
