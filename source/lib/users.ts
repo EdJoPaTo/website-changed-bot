176b583f8b04f0bcb18a691d377d87c252dec0e5
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-09-15 18:00:38 +0200

    refactor(users): migrate to datastore

--- source/lib/users.ts
+++ source/lib/users.ts
@@ -1,52 +1,39 @@
-import {readFileSync, promises as fsPromises} from 'fs'
-
-import * as stringify from 'json-stable-stringify'
+import {KeyValueInMemoryFile} from '@edjopato/datastore'
 
 const USERS_FILE = './persistent/users.json'
 
-export interface User {
-	admin?: boolean;
+export interface UserData {
+	readonly admin: boolean;
 }
 
-let users: Record<number, User>
-try {
-	users = JSON.parse(readFileSync(USERS_FILE, 'utf8'))
-} catch {
-	users = {}
+const users = new KeyValueInMemoryFile<UserData>(USERS_FILE)
+
+if (users.keys().length === 0) {
 	console.error(`The admin has to write the first message to the bot. If someone else is faster, he will be the admin. (Admin is set in "${USERS_FILE}".)`)
 }
 
-async function saveUsersFile(): Promise<void> {
-	await fsPromises.writeFile(USERS_FILE, stringify(users, {space: 2}))
-}
-
 export function getUsers(): readonly number[] {
-	return Object.keys(users).map(id => Number(id))
+	return users.keys().map(id => Number(id))
 }
 
 export function getAdmin(): number {
-	const userId = Object.keys(users)
-		.map(o => Number(o))
-		.find(o => users[o].admin)
+	const userId = users.keys()
+		.find(o => users.get(o)?.admin)
 	if (!userId) {
 		throw new Error('The admin has to write the first message to the bot. If someone else is faster, he will be the admin.')
 	}
 
-	return userId
+	return Number(userId)
 }
 
 export async function addUser(userID: number, isAdmin: boolean): Promise<void> {
-	users[userID] = {admin: isAdmin}
-	return saveUsersFile()
+	await setUserSettings(userID, {admin: isAdmin})
 }
 
-export function getUserSettings(userID: number): User {
-	return users[userID] || {}
+export function getUserSettings(userID: number): UserData | undefined {
+	return users.get(String(userID))
 }
 
-export async function setUserSettings(userID: number, settings: User): Promise<void> {
-	users[userID] = {
-		admin: settings.admin
-	}
-	return saveUsersFile()
+export async function setUserSettings(userID: number, settings: UserData): Promise<void> {
+	await users.set(String(userID), settings)
 }
