951fe4388ed684faa76c99c2e15c08c68074e941
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-09-16 14:29:25 +0200

    feat(group): be annoyed when bot is admin

--- source/group-activity.ts
+++ source/group-activity.ts
@@ -3,14 +3,25 @@ import arrayFilterUnique from 'array-filter-unique'
 
 import {getUserSettings, setUserSettings} from './lib/users'
 
+const removeMeFromBeingAdminMessageText = `Telegram bots which are administrators are a privacy risk to your group as they see every message or might do things every other group admin could do.
+
+As admin bots see every message they require more resources to run which is a useless waste of energy.
+Please change me to be a normal user. 😘`
+
 export const bot = new Composer()
 
 bot.use(async context => {
 	let anyValidAdminExists = false
 
 	try {
+		const botId = context.botInfo!.id
 		const chatAdmins = await context.getChatAdministrators()
-		// TODO: be annoyed when the bot itself is an admin (look at the LockBot for hints)
+
+		if (context.chat!.type === 'supergroup' && chatAdmins.some(o => o.user.id === botId)) {
+			console.log('bot is admin. Be annoyed.', context.chat)
+			await context.reply(removeMeFromBeingAdminMessageText)
+		}
+
 		for (const admin of chatAdmins) {
 			const settings = getUserSettings(admin.user.id)
 			if (!settings) {
