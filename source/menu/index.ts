a05ba1fef14c8a1d53a7a6e14cb7701fb8c81a3e
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-10-21 20:17:31 +0200

    feat(add): add by just sending an url

--- source/menu/index.ts
+++ source/menu/index.ts
@@ -1,5 +1,6 @@
 import {Composer} from 'telegraf'
 import {MenuMiddleware} from 'telegraf-inline-menu'
+import {MessageEntity} from 'typegram'
 
 import {Context} from '../context'
 
@@ -17,4 +18,36 @@ const menuMiddleware = new MenuMiddleware('/', mainMenu)
 bot.command('start', async ctx => menuMiddleware.replyToContext(ctx))
 bot.command('add', async ctx => menuMiddleware.replyToContext(ctx, '/add/'))
 
+bot.on('message', async (ctx, next) => {
+	if (!ctx.message) {
+		throw new Error('typings?!')
+	}
+
+	const text = ctx.message.text ?? ctx.message.caption ?? ''
+	const entities = ctx.message.entities ?? ctx.message.caption_entities ?? []
+
+	const url = getFirstUrlFromMessageEntities(text, entities as any[])
+
+	if (url) {
+		ctx.session.addUrl = url
+		await menuMiddleware.replyToContext(ctx, '/add/')
+	} else {
+		await next()
+	}
+})
+
 bot.use(menuMiddleware.middleware())
+
+function getFirstUrlFromMessageEntities(text: string, entities: readonly MessageEntity[]): string | undefined {
+	for (const entity of entities) {
+		if (entity.type === 'text_link') {
+			return entity.url
+		}
+
+		if (entity.type === 'url') {
+			return text.slice(entity.offset, entity.offset + entity.length)
+		}
+	}
+
+	return undefined
+}
