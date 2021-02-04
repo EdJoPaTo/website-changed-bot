9800dd855077baabbfdc24f0e6167152ecd075a4
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-02-04 15:00:38 +0100

    feat: allow BOT_TOKEN env

--- source/index.ts
+++ source/index.ts
@@ -16,8 +16,13 @@ import {notifyChange, notifyError, init as initNotifyTgUser} from './notify-tg-u
 
 process.title = 'website-changed-bot'
 
-const tokenFilePath = existsSync('/run/secrets') ? '/run/secrets/bot-token.txt' : 'bot-token.txt'
-const token = readFileSync(tokenFilePath, 'utf8').trim()
+const token = (existsSync('/run/secrets/bot-token.txt') && readFileSync('/run/secrets/bot-token.txt', 'utf8').trim()) ||
+	(existsSync('bot-token.txt') && readFileSync('bot-token.txt', 'utf8').trim()) ||
+	process.env.BOT_TOKEN
+if (!token) {
+	throw new Error('You have to provide the bot-token from @BotFather via file (bot-token.txt) or environment variable (BOT_TOKEN)')
+}
+
 const bot = new Telegraf<Context>(token)
 
 if (process.env.NODE_ENV !== 'production') {
